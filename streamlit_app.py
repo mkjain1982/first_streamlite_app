b33ae5ba79f39ff076c6f899eed6cd9f5476cbab
mkjain1982 <mkjain1982@yahoo.com>
2023-08-12 22:28:01 +0530

    Update streamlit_app.py
    Second Step

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My First Streamlit App')
+streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale  Spinach & rocket Smoothy')
+streamlit.text('Hard Boiled  Free range')
+
