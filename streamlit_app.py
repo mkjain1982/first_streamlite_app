3c4bf52574fbf8c76866e516da62d7a48a2c0566
mkjain1982 <mkjain1982@yahoo.com>
2023-08-26 23:40:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,6 @@ streamlit.text(' 🐔 Hard Boiled  Free range')
 streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -31,13 +29,13 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   # streamlit.write('The user entered ', fruit_choice)
-if not fruit_choice :
-  streamlit.error("Please select a fruit to get informtion")
-else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-  # Normalize the json data
-  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get informtion")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    # Normalize the json data
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
@@ -50,7 +48,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_row)
-
 
 fruit_list_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thank you for adding ', fruit_list_choice)
